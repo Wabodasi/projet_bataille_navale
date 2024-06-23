9a251b1b55af94a52fe01ba4703488bd3d3f0ae5
WABO MBOBDA <wabodasi@gmail.com>
2024-06-23 12:36:00 +0100

    ajout du FootMenu et la possibbilite de chanber de terrain de jeu

--- Bataille navale/composants/element.tsx
+++ Bataille navale/composants/element.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {EventHandler, useLayoutEffect, useRef, useState} from 'react';
-import {Animated, Dimensions, PanResponder, StyleSheet, Text, ToastAndroid, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
+import {Animated, Button, Dimensions, PanResponder, StyleSheet, Text, ToastAndroid, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
 import { EventArgs } from 'react-native/Libraries/Performance/Systrace';
 import { Float, Int32 } from 'react-native/Libraries/Types/CodegenTypes';
 import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
@@ -8,6 +8,8 @@ import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
 const {width, height} = Dimensions.get("window")
 const desiredHeight: Float = height * 0.9;
 const desiredWidth: Float = width * 1;
+const heightFootmenu: Float = height - desiredHeight
+const WidthFootmenu: Float = width * 1
 
 
 interface PionPosition {
@@ -20,7 +22,7 @@ function El(): React.JSX.Element {
 
   const [position, setPosition] = useState({x: 0,y: 0});
   const [pionPositions, setPionPositions] = useState<PionPosition[]>([]);
-
+  const [terrainVisible, setTerrainVisible] = useState<Boolean>(true)
 
   const restreindre = (pos: Float, maxPos: Float, minPos: Float) => {
     
@@ -36,82 +38,53 @@ function El(): React.JSX.Element {
     return pos;
   }
 
-
-  // const pan = useRef(new Animated.ValueXY()).current;
-
-  // const panResponder = useRef(
-  //   PanResponder.create({
-  //     onMoveShouldSetPanResponder: () => true,
-  //     onPanResponderMove: Animated.event([null, {dx: pan.x, dy: pan.y}]),
-  //     onPanResponderRelease: () => {
-  //       pan.extractOffset();
-  //     },
-  //   }),
-  // ).current;
-
-  // const panResponder2 = useRef(
-  //   PanResponder.create({
-  //     onStartShouldSetPanResponder : () => true,
-  //     onPanResponderMove: (event, gestureState) => 
-  //     {
-  //       setPosition({x: gestureState.moveX + position.x  , y: gestureState.moveY + position.y })
-  //     },
-  //     onPanResponderRelease: (event, gestureState) => {
-        
-  //     },
-  //     onPanResponderGrant: (event, gestureState) => {
-        
-  //     }
-  //   })
-  // ).current
-
-  // const handleTap = (event: any) => {
-  //   const { locationX, locationY } = event.nativeEvent;
-  //   setPosition({ x: restreindre(locationX, desiredWidth, 0), y: restreindre(locationY, desiredHeight, 0) });
-  // };
-
   const handlePress = (event: any) => {
     const { locationX, locationY } = event.nativeEvent;
     // Ajoutez la position du pion à l'état
     setPionPositions([...pionPositions, { x: restreindre(locationX, desiredWidth, 0), y: restreindre(locationY, desiredHeight, 0) }]);
   };
 
+  const handleChangeTerrain = () =>
+  {
+    setTerrainVisible(!terrainVisible)
+  }
 
   return (
-    // <View style={{backgroundColor:  "#011111" , height: 650, width: 500}}>
-    //   <Animated.View style={[styles.element,{transform: [{translateX: pan.x}, {translateY: pan.y}]}]}
-    //   {...panResponder.panHandlers}>
-    //     <Text >hhhvv</Text>
-    //   </Animated.View>
-    // </View>
-
-    // <View {...panResponder2.panHandlers} style={{backgroundColor: "#011111", height: 650, width: 500, position: "relative"}}>
-    //   <Animated.View style={{left: position.x, top: position.y}}>
-    //     <Text>
-    //       waboasdasdas
-    //     </Text>
-    //   </Animated.View>
-    // </View>
-    //  <View style={styles.container}>
-    //   <TouchableOpacity onPress={handleTap} style={{backgroundColor: "#011111", height: desiredHeight, width: desiredWidth}}>
-    //     <View  style={[styles.element,{top: position.y, left: position.x}]}/>
-    //   </TouchableOpacity>
-    // </View>
+    
 
     
     <View style={styles.container}>
       
-      <TouchableOpacity
+      {terrainVisible && <TouchableOpacity
         style={{backgroundColor: "#011111", height: desiredHeight, width: desiredWidth}}
         onPress={handlePress}>
 
-        {pionPositions.map((pion, index) => (
-        <View
-          key={index}
-          style={[styles.element, { left: pion.x, top: pion.y }]}
-        />
-      ))}
-      </TouchableOpacity>
+          {pionPositions.map((pion, index) => (
+          <View
+            key={index}
+           style={[styles.element, { left: pion.x, top: pion.y }]}
+          />
+
+        ))}
+      
+      </TouchableOpacity>}
+      
+      {!terrainVisible && <TouchableOpacity
+        style={{backgroundColor: "#01110", height: desiredHeight, width: desiredWidth}}>
+
+      </TouchableOpacity>}
+
+      <View style={{height: heightFootmenu, width: WidthFootmenu, backgroundColor: "white"}}>
+        
+      <Button
+        onPress={handleChangeTerrain}
+        title="changer de terrain"
+        color="#841584"
+        accessibilityLabel="En savoir plus sur ce bouton violet"
+      />
+        
+      </View>
+
     </View>
 
     
@@ -119,32 +92,7 @@ function El(): React.JSX.Element {
 }
 
 
-// const styles = StyleSheet.create({
-//   container:{
-//     flex: 1,
-//     position: "relative"
 
-//   },
-//   element:{
-//     position: "absolute",
-//     width: 100    
-//   },
-//   container2: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   touchableArea: {
-//     backgroundColor: 'blue',
-//     padding: 20,
-//     borderRadius: 10,
-//   },
-//   fullScreen:
-//   {
-//     flex: 1
-//   }
-    
-// })
 
 const styles = StyleSheet.create({
   container: {
