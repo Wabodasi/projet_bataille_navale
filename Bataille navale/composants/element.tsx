3f423bd20d889927392e6f7259ab6b0cdf171b60
WABO MBOBDA <wabodasi@gmail.com>
2024-06-24 18:43:43 +0100

    - module cpu pour placer les pions automatiquement -module pour placer les croix sur les 2 terrain -module pour decter si le joueur a touche un pion -...

--- Bataille navale/composants/element.tsx
+++ Bataille navale/composants/element.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, {EventHandler, useEffect, useLayoutEffect, useRef, useState} from 'react';
-import {Animated, Button, Dimensions, PanResponder, StyleSheet, Text, ToastAndroid, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
+import {Alert, Animated, Button, Dimensions, PanResponder, StyleSheet, Text, ToastAndroid, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native';
 import { EventArgs } from 'react-native/Libraries/Performance/Systrace';
 import { Float, Int32 } from 'react-native/Libraries/Types/CodegenTypes';
 import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
@@ -10,12 +10,24 @@ const desiredHeight: Float = height * 0.9;
 const desiredWidth: Float = width * 1;
 const heightFootmenu: Float = height - desiredHeight
 const WidthFootmenu: Float = width * 1
-
+const TERRAIN_DE_JEUX1 = 1
+const TERRAIN_DE_JEUX2 = 2
+enum GameStates{
+  PLACE_PIONS,
+  POSITIONNE_CROIX,
+  ATTEND_JOUEUR_ADVERSE,
+  ANIMATION,
+  JEU_TERMINE,
+  DEBUT_DU_JEU
+}
+let state = GameStates.DEBUT_DU_JEU
 
 interface PionPosition {
   x: number;
   y: number;
 }
+
+
 
 // Utiliser pour restreindre  la position d'un element 
 const restreindre = (pos: Float, maxPos: Float, minPos: Float) => {
@@ -62,8 +74,13 @@ function El(): React.JSX.Element {
   const [position, setPosition] = useState({x: 0,y: 0});
   const [pionPositions, setPionPositions] = useState<PionPosition[]>([]);
   const [pionPositions2, setPionPositions2] = useState<PionPosition[]>([]);
+  const [croixPositions1, setCroixPositions1] = useState<PionPosition[]>([]);
+  const [croixPositions2, setCroixPositions2] = useState<PionPosition[]>([]);
+  
   const [terrainVisible, setTerrainVisible] = useState<Boolean>(true)
+  const [currentGameState, setCurrentGameState] = useState(GameStates.PLACE_PIONS) 
   const nombreMaxPion = 5;
+  
 
 
   // Utiliser pour positionner les pions
@@ -71,28 +88,58 @@ function El(): React.JSX.Element {
     const { locationX, locationY } = event.nativeEvent;
     // Ajoutez la position du pion à l'état
 
-    if(pionPositions.length == nombreMaxPion)
+    if(state === GameStates.PLACE_PIONS)
     {
-      return
+      if(pionPositions.length == nombreMaxPion)
+      {
+        state = GameStates.POSITIONNE_CROIX
+      }
+      else
+      {
+        setPionPositions([...pionPositions, 
+          { x: restreindre(locationX, desiredWidth, 0), y: restreindre(locationY, desiredHeight, 0) }]);
+      }
+      
     }
 
-    setPionPositions([...pionPositions, { x: restreindre(locationX, desiredWidth, 0), y: restreindre(locationY, desiredHeight, 0) }]);
+    
+    
   }
+  const handlePressPosCroix = (event: any) =>
+  {
+    // recuperation des coodonnees ecrans
+    const { locationX, locationY } = event.nativeEvent;
+
+    if(state == GameStates.POSITIONNE_CROIX)
+    {
+      
+      if( croixPositions2.length >= 1)
+      {
+        return
+      }
+      setCroixPositions2 ([...croixPositions2, 
+        { x: restreindre(locationX, desiredWidth, 0), y: restreindre(locationY, desiredHeight, 0) }]);
+    }
+    
+    
+
+  }
+
 
   // Utiliser pour gerer l'affichage des terrains
   const handleChangeTerrain = () =>
   {
     setTerrainVisible(!terrainVisible)
   }
-
-  // Utiliser pour ajouter les pion de maniere aleatoire
+  
+  // Utiliser pour ajouter les pions de maniere aleatoire
   const handlerAutoAjoutPion = () => {
     if (pionPositions2.length === nombreMaxPion) {
       return; // Si l'array est déjà plein, sortir de la fonction
     }
-  
+    
     const newPionPositions = []; // Créer un nouvel array pour stocker les nouvelles positions
-  
+    
     for (let i = 0; i < nombreMaxPion; i++) {
       const pos = getPositionAleatoire(0, desiredWidth, 0, desiredHeight);
       newPionPositions.push({ x: restreindre(pos.x, desiredWidth, 0), y: restreindre(pos.y, desiredHeight, 0) }); // Ajouter la nouvelle position à l'array temporaire
@@ -101,12 +148,103 @@ function El(): React.JSX.Element {
     setPionPositions2([...pionPositions2, ...newPionPositions]); // Ajouter toutes les nouvelles positions à l'array existant
   };
 
+  // Utiliser pour effacer les pions sur un terrain
+  const resetTerrain = (terrainDejeux: number) => {
+    
+    if(terrainDejeux === TERRAIN_DE_JEUX1)
+    {
+      setPionPositions([])
+    }
+    else if(terrainDejeux === TERRAIN_DE_JEUX2)
+    {
+      setPionPositions2([])
+    }
+    
+    
+  }
+  
+  // Utiliser pour dire si 2 elements se touchent 
+  function pionsSeTouchent(
+    pion1: PionPosition, 
+    pion2: PionPosition, 
+    rayon1: number, rayon2: number): boolean {
+      
+      const distanceEntrePions = Math.sqrt((pion2.x - pion1.x) ** 2 + (pion2.y - pion1.y) ** 2);
+      const sommeRayons = rayon1 + rayon2;
+      
+      return distanceEntrePions <= sommeRayons;
+    }
+    
+  // utiliser pour savoir si un joueur a toucher des pions
+  function hitPions()
+  {
+    for(let i = 0; i < pionPositions2.length; i++)
+    {
+      let touche = pionsSeTouchent(croixPositions2[0], pionPositions2[i], 25, 25) 
+      if(touche)
+      {
+        ToastAndroid.show("Touche", ToastAndroid.SHORT);    
+        return
+      }
+    }
+  }
+
+  // Utiliser pour simuler l'action du 2en joueur(CPU)
+  function cpuPlay()
+  {
+    ToastAndroid.show("CPU", ToastAndroid.SHORT)
+    if( croixPositions1.length >= 1)
+      {
+        // ici on change d'etat
+        return
+      }
+
+      // generation d'une position aleatoire
+      const pos = getPositionAleatoire(0, desiredWidth, 0, desiredHeight);
+      // ajout de la position
+      setCroixPositions1 ([...croixPositions1, 
+        { x: restreindre(pos.x, desiredWidth, 0), y: restreindre(pos.y, desiredHeight, 0) }]);
+  }
+  
+
+  function Dialog()
+  {
+    if(state == GameStates.DEBUT_DU_JEU)
+    {
+      Alert.alert("DEBUT DU JEU", "Positionne tes pions sur le terrain")
+      state = GameStates.PLACE_PIONS
+    }
+    
+  }
+  function Dialog1()
+  {
+    if(state == GameStates.POSITIONNE_CROIX)
+    {
+      Alert.alert("DEBUT DU JEUssdd", "Positionne tes pions sur le terrain")
+      state = GameStates.DEBUT_DU_JEU
+    }
+    
+  }
+
+  function setState(etat:number)
+  {
+    state = etat
+  }
+
+  // Declaration des UseEffects
+  useEffect( () => {
+    hitPions()
+  }
+    , [croixPositions2])
+  
+    
+  Dialog()
   
   return (
     
-    
+  
     <View style={styles.container}>
-      
+
       {/* terrain de jeu1  */}
       {terrainVisible && <TouchableOpacity
         style={{backgroundColor: "#011111", height: desiredHeight, width: desiredWidth}}
@@ -118,12 +256,20 @@ function El(): React.JSX.Element {
             key={index}
             style={[styles.element, { left: pion.x, top: pion.y }]}/>
           ))}
+
+        {croixPositions1.map((pion, index) => (    
+          <View
+          key={index}
+          style={[styles.element3, { left: pion.x, top: pion.y }]}/>
+
+        ))}
       
       </TouchableOpacity>}
 
       {/* terrain de jeu 2 */}
       {!terrainVisible && <TouchableOpacity
-        style={{backgroundColor: "#01110", height: desiredHeight, width: desiredWidth}}>
+        style={{backgroundColor: "#01110", height: desiredHeight, width: desiredWidth}}
+        onPress={handlePressPosCroix}>
 
         {pionPositions2.map((pion, index) => (
           
@@ -132,6 +278,15 @@ function El(): React.JSX.Element {
           style={[styles.element2, { left: pion.x, top: pion.y }]}/>
 
         ))}
+
+        {croixPositions2.map((pion, index) => (
+          
+          <View
+          key={index}
+          style={[styles.element3, { left: pion.x, top: pion.y }]}/>
+
+        ))}
+        
         
 
       </TouchableOpacity>}
@@ -150,6 +305,7 @@ function El(): React.JSX.Element {
         color="#841584"
         accessibilityLabel="En savoir plus sur ce bouton violet"
       />
+
         
       </View>
 
@@ -180,6 +336,13 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     position: 'absolute',
   },
+  element3: {
+    width: 25,
+    height: 25,
+    backgroundColor: 'orange',
+    position: 'absolute',
+  },
+
 });
 
 export default El;
